859a18d715ddba99ba3d3473cf4d355c81d66ab9
marcoscl84 <marcoscontelima@yahoo.com.br>
2023-06-07 23:03:23 -0300

    fix: solução para tailwind funcionar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import "./styles/global.css";
-import { Header } from "../components/Header";
+// import { Header } from "../components/Header";
+import { SummaryTable } from "../components/SummaryTable";
+import logoImage from "../assets/logo.svg";
+import { Plus } from "phosphor-react";
 
 // import { Habit } from "../components/Habit";
 
@@ -7,7 +10,20 @@ export function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
-        <Header />
+        {/* HEADER */}
+        <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
+          <img src={logoImage} alt="logo" />
+
+          <button
+            type="button"
+            className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300 transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background"
+          >
+            <Plus size={20} className="text-violet-500" />
+            Novo hábito
+          </button>
+        </div>
+        {/* <Header /> */}
+        <SummaryTable />
       </div>
     </div>
   );
